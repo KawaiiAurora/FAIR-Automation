6e54a773fd634fc3628c5440361eeeae941a5c4e
KawaiiAurora <kawaiiaurora@outlook.com>
2021-03-13 19:36:53 +0100

    Authors in Add Publication are dandier

--- BioinformaticsPortal/static/js/add_publication.js
+++ BioinformaticsPortal/static/js/add_publication.js
@@ -5,6 +5,12 @@ function updateTypeName(select){
 
 function addAuthor(){
     let form_idx = $('#id_form-TOTAL_FORMS').val();
-	$('#author_form_set').append($('#empty_form').html().replace(/__prefix__/g, form_idx));
+	$('#author_form_set').append($('#empty_form').html().replace(/__prefix__/g, form_idx).replace('id="author_form_"',`id="author_form_${form_idx}"`).replace('onclick="removeAuthor()"',`onclick="removeAuthor(${form_idx})"`));
 	$('#id_form-TOTAL_FORMS').val(parseInt(form_idx) + 1);
+}
+
+function removeAuthor(){
+    let form_idx = $('#id_form-TOTAL_FORMS').val();
+    $(`#author_form_${form_idx-1}`).remove();
+    $('#id_form-TOTAL_FORMS').val(parseInt(form_idx) - 1);
 }
